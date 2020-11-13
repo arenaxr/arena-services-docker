814dae083826e558db44c49e0a96c85139b76a02
Nuno Pereira <nampereira@gmail.com>
2020-11-13 17:05:32 -0500

    Static files from /user/static/

--- conf/arena-account-settings.py
+++ conf/arena-account-settings.py
@@ -160,7 +160,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/user/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
