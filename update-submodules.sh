4395638cb268a1e1d64576b19ab683032fea8c50
Nuno Pereira <nampereira@gmail.com>
2020-11-13 16:02:18 -0500

    Update submodules supprts different branch names

--- update-submodules.sh
+++ update-submodules.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-submodules=( "ARENA-core" "ARENA-auth" "arena-persist" "arts" "arena-runtime-simulated")
+submodules=( "ARENA-core:master" "ARENA-auth:master" "arena-account:main" "arena-persist:master" "arts:master" "arena-runtime-simulated:master")
 
 for s in "${submodules[@]}"
 do
 	echo $s
-  cd $s
-  git checkout master
+	repo=`echo $s | cut -d':' -f1`
+	branch=`echo $s | cut -d':' -f2`
+  cd $repo
+  git checkout $branch
   git pull
   cd ..
-  git add $s
+  git add $repo
 done
 
 # commit the change in arena-services-docker repo
