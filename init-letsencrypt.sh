669668c110269d54252853a42cf79bbf95910f4b
Nuno Pereira <nampereira@gmail.com>
2020-11-17 02:12:54 +0000

    Minor fix

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -11,8 +11,8 @@ data_path="./data/certbot"
 email=$EMAIL # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
-if [ -d "$data_path" ]; then
-  read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
+if [ -d "$data_path"/conf ]; then
+  read -p "Existing data found. Continue and replace existing certificate? (y/N) " decision
   if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
     exit
   fi
