bc9a7d25ef57e0df414614939b706392450f2f62
Nuno Pereira <nampereira@gmail.com>
2020-11-17 01:00:03 +0000

    Generate all config files from templates

--- init.sh
+++ init.sh
@@ -42,31 +42,15 @@ export $(grep -v '^#' environment.env | xargs)
 export $(grep -v '^#' environment-secret.env | xargs)
 export ESC="$"
 
-echo -e "\n### Creating conf/nginx-conf.d/arena-web.conf from template (conf/templates/arena-web.tmpl)\n"
-mkdir conf/nginx-conf.d 2> /dev/null
-envsubst < conf/templates/arena-web.tmpl > conf/nginx-conf.d/arena-web.conf
+echo -e "\n### Creating config files (conf/*) from templates (conf-templates/*)"
 
-echo -e "\n### Creating conf/arena-web-dev.conf from template (conf/templates/arena-web-dev.tmpl)\n"
-mkdir conf/nginx-conf.d 2> /dev/null
-envsubst < conf/templates/arena-web-dev.tmpl > conf/arena-web-dev.conf
-
-echo -e "\n### Creating conf/mosquitto.conf from template (conf/templates/mosquitto.tmpl)\n"
-envsubst < conf/templates/mosquitto.tmpl > conf/mosquitto.conf
-
-echo -e "\n### Creating conf/mosquitto-br-conn.conf from template (conf/templates/mosquitto-br-conn.tmpl)\n"
-envsubst < conf/templates/mosquitto-br-conn.tmpl > conf/mosquitto-br-conn.conf
-
-echo -e "\n### Creating conf/arena-runtime-simulated.conf from template (conf/templates/arena-runtime-simulated.tmpl)\n"
-envsubst < conf/templates/arena-runtime-simulated.tmpl > conf/arena-runtime-simulated.conf
-
-echo -e "\n### Creating conf/arts-settings.py from template (conf/templates/arts-settings.tmpl)\n"
-envsubst < conf/templates/arts-settings.tmpl > conf/arts-settings.py
-
-echo -e "\n### Creating conf/arena-defaults.js from template (conf/templates/arena-defaults.tmpl)\n"
-envsubst < conf/templates/arena-defaults.tmpl > conf/arena-defaults.js
-
-echo -e "\n### Creating conf/auth-config.json from template (conf/templates/auth-config.tmpl)\n"
-envsubst < conf/templates/auth-config.tmpl > conf/auth-config.json
+for t in $(ls conf-templates/)
+do
+  f="${t%.*}"
+  cp conf/$f conf/$f.bak 2>&1 > /dev/null
+  echo -e "\t conf-templates/$t -> conf/$f"
+  envsubst < conf-templates/$t > conf/$f
+done
 
 echo -e "\n### Init letsencrypt\n"
 ./init-letsencrypt.sh
