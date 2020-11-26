1074053daad49bf6c47446af44668600d2981618
Nuno Pereira <nampereira@gmail.com>
2020-11-26 15:55:51 +0000

    Creating .env from init.env

--- init.sh
+++ init.sh
@@ -18,3 +18,8 @@ fi
 
 echo -e "\n\e[1m### Init letsencrypt (create/renew certificates)\e[0m\n"
 docker run -it --env-file .env --env-file secret.env -e OWNER=`id -u`:`id -g` --rm -v $PWD:/work -v $PWD/data/certbot/conf:/etc/letsencrypt -v $PWD/data/certbot/www:/var/www/certbot -w /work -p 80:80 conixcenter/arena-services-docker-init-utils /work/init-letsencrypt.sh
+if [ $? -ne 0 ]
+then
+    echo -e "\n\e[1m### Init letsencrypt failed. Did you stop services on port 80 ? To stop all services: [./dev.sh | ./prod.sh ./staging.sh] down).\e[0m\n"
+    exit 1
+fi
