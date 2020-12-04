9ff998140f8ef5a9f8f27d4e39c90c5cd0fe98fa
Nuno Pereira <nampereira@gmail.com>
2020-12-04 20:39:35 +0000

    Copy arena client defaults

--- dev.sh
+++ dev.sh
@@ -4,4 +4,7 @@
 # make sure ARENA-core/conf folder exists
 [ ! -d "ARENA-core/conf" ] && mkdir ARENA-core/conf
 
+# copy arena client defaults
+cp ./conf/arena-web-conf/arena-defaults-dev.js ./conf/arena-web-conf/defaults.js
+
 docker-compose -f docker-compose.override.yaml $@ # = docker-compose $@
--- prod.sh
+++ prod.sh
@@ -4,4 +4,7 @@
 # make sure ARENA-core/conf folder exists
 [ ! -d "ARENA-core/conf" ] && mkdir ARENA-core/conf
 
+# copy arena client defaults
+cp ./conf/arena-web-conf/arena-defaults.js ./conf/arena-web-conf/defaults.js
+
 docker-compose -f docker-compose.yaml -f docker-compose.prod.yaml $@
