1368c0bc500995130ca04a00782fbce60bb2b016
Nuno Pereira <nampereira@gmail.com>
2020-12-05 18:02:41 -0500

    Create account static folder in webserver

--- init.sh
+++ init.sh
@@ -7,6 +7,9 @@ then
   cp init.env .env
 fi
 
+# TMP: create ARENA-core/user/static
+[ ! -d "ARENA-core/user/static" ] mkdir -p ARENA-core/user/static
+
 echo -e "\n\e[1m### Init config files (create secrets.env, ./conf/* files, and ./data/* folders)\e[0m\n"
 docker run -it --env-file .env --env-file secret.env -e OWNER=`id -u`:`id -g` --rm -v $PWD:/work -w /work conixcenter/arena-services-docker-init-utils /work/init-config.sh
 
