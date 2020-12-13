c3f7e2a0b742a95d9a15500e5cd18c96d9553f29
Nuno Pereira <nampereira@gmail.com>
2020-12-13 15:46:31 -0500

    Fix Backup

--- backup.sh
+++ backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+source .env
 SCRIPT_PATH=$(dirname $(realpath -s $0))
-ARENA_DOCKER_FOLDER=/home/wiselab/arena-services-docker
 [ ! -d "$SCRIPT_PATH/data/backup" ] && mkdir $SCRIPT_PATH/data/backup
-docker exec -it arena-services-docker_mongodb_1 sh -c 'exec mongodump --db arena_persist --collection arenaobjects --out -' > $SCRIPT_PATH/data/backup/mongo-arenaobjects-$(date +%j).dump
-cp $SCRIPT_PATH/data/arena-store/database.db $SCRIPT_PATH/data/backup/
-
+sudo /usr/bin/docker exec -it arena-services-docker_mongodb_1 sh -c 'exec mongodump --db arena_persist --collection arenaobjects --out - > /backup/mongo-arenaobjects.dump'
+cp $SCRIPT_PATH/data/arena-store/database.db $SCRIPT_PATH/data/backup/store-database.db
+chown -R $BACKUP_USER $SCRIPT_PATH/data/backup/*
