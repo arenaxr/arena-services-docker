47e132f06e3dbd8e2c8d95c4ff26d0e664533dcf
Nuno Pereira <nampereira@gmail.com>
2021-01-08 20:23:48 +0000

    Mongodb backup to folder

--- backup.sh
+++ backup.sh
@@ -2,6 +2,6 @@
 source .env
 SCRIPT_PATH=$(dirname $(realpath -s $0))
 [ ! -d "$SCRIPT_PATH/data/backup" ] && mkdir $SCRIPT_PATH/data/backup
-sudo /usr/bin/docker exec -it arena-services-docker_mongodb_1 sh -c 'exec mongodump --db arena_persist --collection arenaobjects --out - > /backup/mongo-arenaobjects.dump'
+sudo /usr/bin/docker exec -it arena-services-docker_mongodb_1 sh -c 'exec mongodump --db arena_persist --collection arenaobjects --out /backup/mongo'
 cp $SCRIPT_PATH/data/arena-store/database.db $SCRIPT_PATH/data/backup/store-database.db
 chown -R $BACKUP_USER $SCRIPT_PATH/data/backup/*
