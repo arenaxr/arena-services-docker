ea4427522054af04a4a4ebbc7079353e5390410c
Nuno Pereira <nampereira@gmail.com>
2021-02-10 00:14:54 +0000

    Update arts

--- prod.sh
+++ prod.sh
@@ -7,4 +7,11 @@
 # copy arena client defaults
 cp ./conf/arena-web-conf/arena-defaults.js ./conf/arena-web-conf/defaults.js
 
+# force static volumes to be created again on "up"
+if [[ "$*" == *up* ]]
+then
+    docker volume rm arena-services-docker_arts-static-content
+    docker volume rm arena-services-docker_account-static-content
+fi
+
 docker-compose -f docker-compose.yaml -f docker-compose.prod.yaml $@
