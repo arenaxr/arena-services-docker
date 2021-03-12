899921d6a515333510ca6f2ac719952d2d023006
Nuno Pereira <nampereira@gmail.com>
2021-03-12 16:57:28 +0000

    Go to initial working directory on error

--- build-arena-core.sh
+++ build-arena-core.sh
@@ -4,6 +4,7 @@ echo -e "\n\e[1m### Building ARENA core js\e[0m\n"
 read -p "Continue ? (y/N) " -r
 
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-    [ -x "$(command -v git)" ] && cd ARENA-core && git checkout master && git pull && cd ..
+    CPWD=$PWD
+    [ -x "$(command -v git)" ] && cd ARENA-core && git checkout master && git pull && cd .. || cd $CPWD
     docker run -it -e OWNER=`id -u`:`id -g` --rm -v $PWD/ARENA-core:/ARENA-core -w /ARENA-core conixcenter/arena-services-docker-init-utils npm run build
 fi
