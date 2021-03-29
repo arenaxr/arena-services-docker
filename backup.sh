63dec202b1d44dcc9562f77b33e1d6317bd49765
Nuno Pereira <nampereira@gmail.com>
2021-03-29 22:48:06 +0000

    Backup to host folder

--- backup.sh
+++ backup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 source .env
-BACKUP_PATH=$(realpath -s $ARENA_DOCKER_REPO_FOLDER)/data/backup 
+BACKUP_PATH=$(realpath -s $ARENA_DOCKER_REPO_FOLDER)/data/backup/$HOSTNAME 
 DATA_PATH=$(realpath -s $ARENA_DOCKER_REPO_FOLDER)/data 
 [ ! -d "$BACKUP_PATH" ] && mkdir $BACKUP_PATH
 BFOLDER=$BACKUP_PATH/mongodb
 [ ! -d "$BFOLDER" ] && mkdir $BFOLDER
 # backup mongodb using mongodump
-sudo /usr/bin/docker exec -it arena-services-docker_mongodb_1 sh -c 'exec mongodump --db arena_persist --collection arenaobjects --out /backup/mongodb'
+sudo /usr/bin/docker exec -it -e HOSTNAME=$HOSTNAME arena-services-docker_mongodb_1 sh -c 'exec mongodump --db arena_persist --collection arenaobjects --out /backup/$HOSTNAME/mongodb'
 # backup other services by copying files
 DATA_FOLDERS=( "arena-store" "grafana" "prometheus" "account" )
 for d in "${DATA_FOLDERS[@]}"
