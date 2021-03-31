62ad2de4a008d0f9f5ba6062a61035613720dcbc
Nuno Pereira <nampereira@gmail.com>
2021-03-31 13:40:04 +0000

    Add redirect for additional hostnames

--- init-config.sh
+++ init-config.sh
@@ -95,6 +95,47 @@ do
   chown $OWNER conf/$f
 done
 
+# add server block to redirect additional hostnames
+if [[ ! -z "$ADDITIONAL_HOSTNAMES" ]]; then
+        TMPFN=/tmp/nginx_tmpcfg
+        cat > $TMPFN <<  EOF
+
+server {
+    server_name         $ADDITIONAL_HOSTNAMES;
+    server_tokens off;
+    client_max_body_size 1000M;
+
+    listen              443 ssl;
+    ssl_certificate     /etc/letsencrypt/live/arenaxr.org/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/arenaxr.org/privkey.pem;
+    include             /etc/letsencrypt/options-ssl-nginx.conf;
+    ssl_dhparam         /etc/letsencrypt/ssl-dhparams.pem;
+
+    location ^~ /user/ {
+        add_header 'Access-Control-Allow-Origin' "$http_origin";
+        add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS, DELETE, PUT';
+        add_header 'Access-Control-Allow-Credentials' 'true';
+        add_header 'Access-Control-Allow-Headers' 'User-Agent,Keep-Alive,Content-Type';
+        proxy_pass http://arena-account:8000;
+        proxy_http_version 1.1;
+        proxy_set_header Host $host;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection "Upgrade";
+        proxy_read_timeout 86400;
+    }
+
+    location / {
+        return 301 https://$HOSTNAME$request_uri;
+    }
+}
+EOF
+        # add server block to production and staging
+        cat $TMPFN >> ./conf/arena-web.conf
+        cat $TMPFN >> ./conf/arena-web-staging.conf
+        rm $TMPFN
+done
+
+# add server block to redirect jitsi requests
 if [[ ! -z "$JITSI_HOSTNAME" ]]; then
     echo -e "\n### If you are going to setup a Jitsi server on this machine, you will configure nginx to redirect http requests to a Jitsi virtual host (JITSI_HOSTNAME is an alias to the IP of the machine)."
     read -p "Add server block to redirect requests to Jitsi ? (y/N) " -r
@@ -102,6 +143,7 @@ if [[ ! -z "$JITSI_HOSTNAME" ]]; then
         TMPFN=/tmp/nginx_tmpcfg
         JITSI_HOSTNAME_NOPORT=$(echo $JITSI_HOSTNAME | cut -f1 -d":")
         cat > $TMPFN <<  EOF
+
 server {
     server_name         $JITSI_HOSTNAME_NOPORT;
     listen              80;
