7b2b81f0e91df062b508f7ada1ba147f16c73dba
Nuno Pereira <nampereira@gmail.com>
2021-04-06 23:39:36 +0000

    Small clarification to messages

--- init-utils/build-push-container.sh
+++ init-utils/build-push-container.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+DOCKER_USER=conixcenter
 docker build . -t conixcenter/arena-services-docker-init-utils
 
-echo -e "\n### Push the container to dockerhub (needs credentials)."
+echo -e "\n### Push the container to dockerhub (needs dockerhub credentials for user '$DOCKER_USER'."
 read -p "Continue? (y/N) " -r
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-    docker login --username conixcenter
+    echo "Enter dockerhub credentials for '$DOCKER_USER'"
+    docker login --username $DOCKER_USER
     docker push conixcenter/arena-services-docker-init-utils
 fi
