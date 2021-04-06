0460aca5124034df679d921ae17cbe39e9a09de4
Nuno Pereira <nampereira@gmail.com>
2021-04-06 15:51:50 +0000

    Update token generation script

--- init-utils/genjwt.py
+++ init-utils/genjwt.py
@@ -3,22 +3,28 @@
 import argparse
 from datetime import datetime, timedelta
 import jwt
+import json
 
-
-def generate_token(username, keypath):
+def generate_token(username, days, keypath, jsonOut):
     now = datetime.utcnow()
     claim = {
         "sub": username,
         "subs": ["#"],
         "publ": ["#"],
         'iat': now,
-        'exp': now + timedelta(days=365)
+        'exp': now + timedelta(days=days)
     }
     with open(keypath, 'r') as keyfile:
         key = keyfile.read()
     token = jwt.encode(claim, key, algorithm='RS256')
-    print(token.decode())
-
+    outStr = token.decode()
+    if jsonOut:
+        jsonOutObj = {
+            "username": username,
+            "token": outStr
+        }
+        outStr=json.dumps(jsonOutObj)
+    print(outStr)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=(
@@ -26,5 +32,9 @@ if __name__ == '__main__':
     parser.add_argument('username', help='MQTT username for this service')
     parser.add_argument('-k', dest='keypath', default="mqtt.pem",
                         help='Private RSA key file to use (default "mqtt.pem")')
+    parser.add_argument('-d', dest='days', type=int, default="365",
+                        help='Number of days the token will be valid')
+    parser.add_argument('-j', dest='json', action='store_true', default=False,
+                        help='Generate json with username')    
     args = parser.parse_args()
-    generate_token(args.username, args.keypath)
+    generate_token(args.username, args.days, args.keypath, args.json)    
