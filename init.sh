9fa2e8dcbf1ec80504da7f8219c031e0988b80fa
Nuno Pereira <nampereira@gmail.com>
2021-06-03 21:02:56 +0000

    Create secret.env, if does not exist

--- init.sh
+++ init.sh
@@ -11,6 +11,7 @@ fi
 # build arena-core js
 ./build-arena-core.sh 
 
+touch secret.env &>/dev/null
 echo -e "\n\e[1m### Init config files (create secrets.env, ./conf/* files, and ./data/* folders)\e[0m\n"
 docker run -it --env-file .env --env-file secret.env -e OWNER=`id -u`:`id -g` --rm -v $PWD:/work -w /work conixcenter/arena-services-docker-init-utils /work/init-config.sh
 
