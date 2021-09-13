0bf3cd59e0fe1e0e21ced0f7976c1a3b6e235546
Nuno Pereira <nampereira@gmail.com>
2021-09-13 17:14:01 +0000

    Fix arena-core build files permissions

--- build-arena-core.sh
+++ build-arena-core.sh
@@ -6,5 +6,6 @@ read -p "Build js ? (y/N) " -r
 if [[ $REPLY =~ ^[Yy]$ ]]; then
     CPWD=$PWD
     [ -x "$(command -v git)" ] && cd ARENA-core && git checkout master && git pull && cd .. || cd $CPWD
-    docker run -it -e OWNER=`id -u`:`id -g` --rm -v $PWD/ARENA-core:/ARENA-core -w /ARENA-core conixcenter/arena-services-docker-init-utils npm run build
+    docker run -it --rm -v $PWD/ARENA-core:/ARENA-core -w /ARENA-core conixcenter/arena-services-docker-init-utils npm run build
+    chown `id -u`:`id -g` ./data/keys/jwt.public*
 fi
