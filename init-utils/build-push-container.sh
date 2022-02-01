e8fc54cce3658c9999035d89500b5382ca6f2d67
Nuno Pereira <nampereira@gmail.com>
2022-02-01 02:42:09 +0000

    fix: add init utils version

--- init-utils/build-push-container.sh
+++ init-utils/build-push-container.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+# usage: ./build-push-container.sh <tag>; e.g. ./build-push-container.sh v1.0.0
 DOCKER_USER=conixcenter
-docker build . -t conixcenter/arena-services-docker-init-utils
+docker build . -t conixcenter/arena-services-docker-init-utils:${1:-latest}
 
 echo -e "\n### Push the container to dockerhub (needs dockerhub credentials for user '$DOCKER_USER'."
 read -p "Continue? (y/N) " -r
 if [[ $REPLY =~ ^[Yy]$ ]]; then
     echo "Enter dockerhub credentials for '$DOCKER_USER'"
     docker login --username $DOCKER_USER
-    docker push conixcenter/arena-services-docker-init-utils
+    docker push conixcenter/arena-services-docker-init-utils:${1:-latest}
 fi
