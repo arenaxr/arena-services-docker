612301953ed212f186be89a8522ff2a515f09027
Nuno Pereira <nampereira@gmail.com>
2022-04-22 10:56:01 +0000

    fix: reset ownership of public keys

--- init-config.sh
+++ init-config.sh
@@ -31,9 +31,11 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
   openssl genrsa -out ./data/keys/jwt.priv.pem 4096
   openssl rsa -in ./data/keys/jwt.priv.pem -pubout -outform PEM -out ./data/keys/jwt.public.pem
   openssl rsa -in ./data/keys/jwt.priv.pem -RSAPublicKey_out -outform DER -out ./data/keys/jwt.public.der # mqtt auth plugin requires RSAPublicKey format
-  # change ownership public keys
-  chown $OWNER ./data/keys/jwt.public*
 fi
+
+# reset ownership of public keys
+[ -f ./data/keys/jwt.public.pem ] && chown $OWNER ./data/keys/jwt.public.pem
+[ -f ./data/keys/jwt.public.der ] && chown $OWNER ./data/keys/jwt.public.der
 
 rm ./conf/arena-web-conf/*.pem 2>/dev/null
 # copy public key to /conf/sha256(hostname).pem to be used for Atlassian Service Authentication Protocol (ASAP)
