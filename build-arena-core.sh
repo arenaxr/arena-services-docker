1df41bb38952e5a39b88b522c1894fc1812e22d2
Nuno Pereira <nampereira@gmail.com>
2022-04-22 10:29:44 +0000

    fix: create dist/

--- build-arena-core.sh
+++ build-arena-core.sh
@@ -5,7 +5,7 @@ read -p "Build js (production instances should skip this step) ? (y/N) " -r
 
 if [[ $REPLY =~ ^[Yy]$ ]]; then
     CPWD=$PWD
-    [ -x "$(command -v git)" ] && cd ARENA-core && git checkout master && git pull && cd .. || cd $CPWD
+    [ -x "$(command -v git)" ] && cd ARENA-core && git checkout master && git pull && mkdir dist && cd .. || cd $CPWD
     docker run -it --rm -v $PWD/ARENA-core:/ARENA-core -w /ARENA-core conixcenter/arena-services-docker-init-utils npm run build
     chown `id -u`:`id -g` ./data/keys/jwt.public*
 fi
