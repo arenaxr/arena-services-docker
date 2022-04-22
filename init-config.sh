7bd059ca3af2d4e8b8153288d28108c25d4c6ec4
Nuno Pereira <nampereira@gmail.com>
2022-04-22 12:57:36 +0000

    fix: clearer messages

--- init-config.sh
+++ init-config.sh
@@ -64,7 +64,7 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
     curl_data="{\"text\":\"New MQTT token for $HOSTNAME\", \"attachments\": [ {\"text\":\"\`\`\`alias ${alias_name}_pub='mosquitto_pub -h $HOSTNAME -p 8883 -u $username -P $cli_token'\`\`\`\"}, {\"text\":\"\`\`\`alias ${alias_name}_sub='mosquitto_sub -h $HOSTNAME -p 8883 -u $username -P $cli_token'\`\`\`\"} ]}"
     curl -X POST -H 'Content-type: application/json' --data "$curl_data" $SLACK_DEV_CHANNEL_WEBHOOK
   fi
-  echo -e "\n Service tokens created. !NOTE!: For new service tokens to be used, you need to create config files (reply Y create config files question)."
+  echo -e "\n Service tokens created. !NOTE!: For new service tokens to be used, you need to (re)create config files (answer Y to question about creating config files)."
 fi
 
 # load secrets
@@ -106,7 +106,7 @@ cat .env
 echo
 
 echo -e "Please edit the file .env (shown above) to reflect your setup (hostname, email, ...). \n"
-read -p "Continue and create config files (backups will be created in conf/)? (y/N)" -r
+read -p "Continue and (re)create config files (backups will be created in conf/)? (y/N)" -r
 echo
 if [[ ! $REPLY =~ ^[Yy]$ ]]; then
     echo "Stopped."
