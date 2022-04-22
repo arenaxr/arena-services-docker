a77c322c5223278681d011704feacfa3bfafe172
Nuno Pereira <nampereira@gmail.com>
2022-04-22 10:55:39 +0000

    fix: arena-core build

--- build-arena-core.sh
+++ build-arena-core.sh
@@ -4,8 +4,7 @@ echo -e "\n\e[1m### Building ARENA core js\e[0m\n"
 read -p "Build js (production instances should skip this step) ? (y/N) " -r
 
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-    CPWD=$PWD
-    [ -x "$(command -v git)" ] && cd ARENA-core && git checkout master && git pull && mkdir dist && cd .. || cd $CPWD
-    docker run -it --rm -v $PWD/ARENA-core:/ARENA-core -w /ARENA-core conixcenter/arena-services-docker-init-utils npm run build
-    chown `id -u`:`id -g` ./data/keys/jwt.public*
+    CPWD=${PWD}
+    [ -x "$(command -v git)" ] && cd ARENA-core && git checkout master && git pull && mkdir -p dist
+    docker run -it --rm -v ${CPWD}/ARENA-core:/ARENA-core -w /ARENA-core conixcenter/arena-services-docker-init-utils sh -c "npm install && npm run build"
 fi
