d5c781fd08a68aa75c6a1fbae2ee388c5e10e0d6
nampereira <nampereira@localhost>
2025-04-07 11:16:32 +0000

    Moving files demo.sh build-arena-core.sh cleanup.sh gen-root-token.sh init-certs.sh init-config.sh update-custom-website.sh init.env init.sh VERSION docker-compose.yaml docker-compose.demo.yaml conf-templates/* using master

--- cleanup.sh
+++ cleanup.sh
@@ -1,17 +1,39 @@
 #!/bin/bash
-# cleanup previous config created with init.sh; run with sudo
+# cleanup previous config created with init.sh; **run with sudo**
 
-[ -d conf ] && ( rm -fr conf_bak; mv conf conf_bak && echo "Backup conf in conf_bak" ) || echo "No conf folder found"
-[ -d data ] && ( rm -fr data_bak; mv data data_bak && echo "Backup data in data_bak") || echo "No conf folder found"
-[ -f .env ] && ( rm -fr .env_bak; mv .env .env_bak && echo "Backup .env in .env_bak") || echo "No .env found"
+# load utils
+source init-utils/bash-common-utils.sh 
+
+# handle args
+args=`getopt yntscrbh $*`
+[[ ! $? == 0 ]] && usage
+eval set -- "$args"
+
+# parse options
+while true; do
+    case "$1" in
+        -y)
+            ALWAYS_YES="true"
+            shift
+            ;;
+        -h)
+            echo "$0 [-y]"
+            exit
+            ;;
+        * ) break ;;            
+    esac
+done
+
+[ -d conf ] && ( rm -fr conf_bak 2>&1; mv conf conf_bak && echo "Backup conf in conf_bak" ) || echo "No conf folder found"
+[ -d data ] && ( rm -fr data_bak 2>&1; mv data data_bak && echo "Backup data in data_bak") || echo "No conf folder found"
+[ -f .env ] && ( rm -fr .env_bak 2>&1; mv .env .env_bak && echo "Backup .env in .env_bak") || echo "No .env found"
 
 if [ -d "./data/certbot/conf/live" ]; then
   echo "Existing certificate/letsencrypt data found (deleting and retrying to create certificates might bump into letsencrypt retry limits)."
-  read -p "Continue and remove certificate/letsencrypt files ? (y/N) " decision
-  if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
-    exit
+  readprompt "Continue and remove certificate/letsencrypt files ? (y/N) "
+  if [[ "$REPLY" =~ ^[Yy]$ ]]; then  
+    rm -fr ./data/certbot/conf/*
   fi
-  rm -fr ./data/certbot/conf/*
 else
   echo "No certificate/letsencrypt data found."
 fi
