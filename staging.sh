1c8abc9a46e64837776715deda9f96bf9da483ea
mwfarb <mwfarb@andrew.cmu.edu>
2025-05-20 16:54:39 -0400

    updte staging to docker compose like others

--- staging.sh
+++ staging.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # usage: ./staging.sh [docker-compose SUBCOMMAND: up, down, ...]
 
+# prefer newer docker compose; fall back to older docker-compose
+[[ $(docker compose --help 2>&1) ]] && DOCKER_COMPOSE="docker compose" || DOCKER_COMPOSE="docker-compose"
+[[ $($DOCKER_COMPOSE --help 2>&1) ]] && echo "Docker compose not found. Please install."
+
 if [ $# -eq 0 ]; then
     >&2 echo "No arguments provided. Usage:"
     >&2 echo "$0 [docker-compose SUBCOMMAND: up, down, ...]"
@@ -10,7 +14,7 @@ fi
 if [ ! -d "conf" ] || [ ! -f .env ]; then
     >&2 echo "Config files no found. Did you run ./init.sh ?"
     exit 1
-fi 
+fi
 
 # force static volumes to be created again on "up"
 if [[ "$*" == *up* ]]
@@ -18,9 +22,10 @@ then
     docker volume rm arena-services-docker_account-static-content
 fi
 
-#docker-compose -f docker-compose.yaml -f docker-compose.staging.yaml --env-file VERSION pull -q
+# pull versions in VERSION
+# $DOCKER_COMPOSE -f docker-compose.yaml -f docker-compose.staging.yaml --env-file VERSION pull -q
 
-docker-compose -f docker-compose.yaml -f docker-compose.staging.yaml --env-file VERSION $@
+$DOCKER_COMPOSE -f docker-compose.yaml -f docker-compose.staging.yaml --env-file VERSION $@
 
 if [[ "$*" == *up* ]]
 then
