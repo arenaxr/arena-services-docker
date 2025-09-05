739b7ca95ab7800cdcb18d46063969828767a687
mwfarb <mwfarb@localhost>
2025-09-05 01:58:56 +0000

    Moving files demo.sh build-arena-core.sh cleanup.sh gen-root-token.sh init-certs.sh init-config.sh update-custom-website.sh init.env init.sh VERSION docker-compose.yaml docker-compose.demo.yaml conf-templates/* using master

--- update-custom-website.sh
+++ update-custom-website.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # check if we have our custom website files to write over
 
-[ ! "$(docker ps -a | grep arena-services-docker_arena-web.*)" ] && exit 0 # dont continue if container does not exist
+[ ! "$(docker ps -a | grep arena-services-docker-arena-web.*)" ] && exit 0 # dont continue if container does not exist
 
 echo "Clone custom website repo into a folder named custom-website if you want to override the default website."
 if [ -d "custom-website" ]
 then
     echo "Updating custom website..."
-    docker cp custom-website arena-services-docker_arena-web_1:/usr/share/nginx/html/
-    docker exec arena-services-docker_arena-web_1 sh -c "cp -R /usr/share/nginx/html/custom-website/* /usr/share/nginx/html"
+    docker cp custom-website arena-services-docker-arena-web-1:/usr/share/nginx/html/
+    docker exec arena-services-docker-arena-web-1 sh -c "cp -R /usr/share/nginx/html/custom-website/* /usr/share/nginx/html"
 fi
