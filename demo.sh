a419ea6a1888a929dc92c0180fb716eb55a5c757
hi-liang <hi-liang@localhost>
2026-02-06 18:47:12 +0000

    Moving files demo.sh build-arena-core.sh cleanup.sh gen-root-token.sh init-certs.sh init-config.sh update-custom-website.sh init.env init.sh VERSION docker-compose.yaml docker-compose.demo.yaml conf-templates/* using master

--- demo.sh
+++ demo.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-# usage: ./demo.sh [docker-compose SUBCOMMAND: up, down, ...]
+# usage: ./demo.sh [docker compose SUBCOMMAND: up, down, ...]
 
-# prefer newer docker compose; fall back to older docker-compose
-[[ $(docker compose --help 2>&1) ]] && DOCKER_COMPOSE="docker compose" || DOCKER_COMPOSE="docker-compose"
-[[ $($DOCKER_COMPOSE --help 2>&1) ]] && echo "Docker compose not found. Please install."
+
 
 if [ $# -eq 0 ]; then
     >&2 echo "No arguments provided. Usage:"
-    >&2 echo "$0 [docker-compose SUBCOMMAND: up, down, ...]"
+    >&2 echo "$0 [docker compose SUBCOMMAND: up, down, ...]"
     exit 1
 fi
 
@@ -44,7 +42,7 @@ then
 fi
 
 # pull versions in VERSION
-$DOCKER_COMPOSE -f docker-compose.yaml -f docker-compose.demo.yaml --env-file VERSION pull -q
+docker compose -f docker-compose.yaml -f docker-compose.demo.yaml --env-file VERSION pull -q
 
-$DOCKER_COMPOSE -f docker-compose.yaml -f docker-compose.demo.yaml --env-file VERSION $@
+docker compose -f docker-compose.yaml -f docker-compose.demo.yaml --env-file VERSION $@
 
